d1ace050d68e63cb6b4c321a6035bd84b8fa2f0e
AntoineLet <201830426@ETI-19125.cstfelicien.qc.ca>
2019-09-13 13:32:34 -0400

    master

---
+++ src/Hello/HelloWorld.java
@@ -0,0 +1,37 @@
+package Hello;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args) 
+	{
+		/*int[][] arr = new int[3][2];
+
+	    
+	    arr[0][0] = 1;
+	    arr[0][1] = 2;
+	    arr[1][0] = 3;
+	    arr[1][1] = 4;
+	    arr[2][0] = 5;
+	    arr[2][1] = 6;
+
+	    int max = 1;
+
+	    int x = 0;
+	    int y = 0;
+
+	    for (int b = 0; x < arr.length; x++) {
+	        for (y = 0; y < arr[x].length; y++) {
+	            if (arr[x][y] > max) {
+	                max = arr[x][b];
+	                System.out.println(max);
+	            }
+	            System.out.println(arr[x][y]);
+	        }
+	    }
+
+	    System.out.println(arr[x][y]);*/
+	    
+	    System.out.println("\t test");
+	}
+}
